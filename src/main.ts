aa937677c56dbbd7a45db628da6abe7c17d07654
Cheng <1003306162@qq.com>
2022-05-15 21:37:46 +0800

    feat: 初始化按钮

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,57 @@
+import {
+  unstable_IdlePriority as IdlePriority,
+  unstable_ImmediatePriority as ImmediatePriority,
+  unstable_LowPriority as LowPriority,
+  unstable_NormalPriority as NormalPriority,
+  unstable_UserBlockingPriority as UserBlockingPriority,
+  unstable_getFirstCallbackNode as getFirstCallbackNode,
+  unstable_scheduleCallback as scheduleCallback,
+  unstable_shouldYield as shouldYield,
+  unstable_cancelCallback as cancelCallback,
+  CallbackNode,
+} from 'scheduler'
+
 import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+type Priority =
+  | typeof IdlePriority
+  | typeof ImmediatePriority
+  | typeof LowPriority
+  | typeof NormalPriority
+  | typeof UserBlockingPriority
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+interface Work {
+  priority: Priority
+  count: number
+}
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+const content = document.getElementById('content') as HTMLDivElement
+
+const priority2UseList: Priority[] = [
+  ImmediatePriority,
+  UserBlockingPriority,
+  NormalPriority,
+  LowPriority,
+]
+
+const priority2Name = [
+  'noop',
+  'ImmediatePriority',
+  'UserBlockingPriority',
+  'NormalPriority',
+  'LowPriority',
+  'IdlePriority',
+]
+
+const init = () => {
+  priority2UseList.forEach(priority => {
+    const btn = document.createElement('button')
+    app.appendChild(btn)
+    btn.innerText = priority2Name[priority]
+
+    btn.onclick = () => {}
+  })
+}
+
+init()
